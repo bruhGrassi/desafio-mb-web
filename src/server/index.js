27f91cfa71ad9b48b1678b2d1f77f23333f51d10
bruhGrassi <brunagm88@gmail.com>
2024-06-14 12:28:58 -0300

    chore: add express and server index

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Bruna!");
+});
+
+app.listen(port, () => {
+  console.info(`Server running at http://localhost:${port}`);
+});
