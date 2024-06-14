c83e4fd4a7565bb019317347463c4047014b6bf6
bruhGrassi <brunagm88@gmail.com>
2024-06-14 13:26:50 -0300

    chore: add Button

---
+++ src/client/components/Button.vue
@@ -0,0 +1,63 @@
+<template>
+  <button
+    :class="[
+      'button',
+      { 'button--outline': isOutline, 'button--disabled': disabled },
+    ]"
+    :disabled="disabled"
+    @click="handleClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  isOutline: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["action"]);
+
+const handleClick = () => {
+  emit("action");
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+  width: 100%;
+  height: $size-md;
+  border: 1px solid $secondary-color;
+  cursor: pointer;
+  border-radius: 0.375rem;
+  padding: 0 1rem;
+  background: $secondary-color;
+  color: #fff;
+  font-family: $font-family;
+  font-size: $size-s;
+  font-weight: 300;
+
+  &--outline {
+    background: transparent;
+    color: $secondary-color;
+  }
+
+  &--disabled {
+    background: black;
+    cursor: not-allowed;
+    pointer-events: none;
+  }
+}
+</style>
