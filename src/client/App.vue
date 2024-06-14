eb4bf03265f372c2ffd897433360ab2ba6a5bbef
bruhGrassi <brunagm88@gmail.com>
2024-06-14 13:36:57 -0300

    chore: add InputField

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Button from "@/components/Button.vue";
 import Header from "@/components/Header.vue";
+import InputField from "@/components/InputField.vue";
 </script>
 
 <template>
   <Header title="Seja bem vindo(a)" :actualStep="1" :numOfSteps="4" />
   <Button text="Continuar" :isOutline="false" />
+  <InputField id="name" label="Nome" type="text" />
+  <InputField id="name" label="Nome" type="text" error="Preencha o Nome" />
 </template>
---
+++ src/client/components/InputField.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  label: {
+    type: String,
+  },
+  modelValue: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  id: {
+    type: String,
+  },
+  error: {
+    type: String,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const inputValue = ref(props.modelValue);
+const isFocused = ref(false);
+
+const updateValue = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
+
+<template>
+  <div class="input-field">
+    <label :for="id" class="input-field__label">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :aria-label="label"
+      :class="[
+        'input-field__input',
+        { 'input-field__input--focused': isFocused },
+        { 'input-field__error': error },
+      ]"
+      v-model="inputValue"
+      @input="updateValue"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+    />
+    <span v-if="error" class="input-field__error--text">{{ error }}</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.input-field {
+  width: 100%;
+  font-family: $font-family;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: $size-xs;
+
+  &__label {
+    padding-bottom: 0.375rem;
+    font-size: $size-s;
+    font-weight: 300;
+  }
+
+  &__input {
+    border-radius: 0.375rem;
+    height: $size-md;
+    border: 1px solid;
+    font-size: $size-s;
+    padding-left: $size-xs;
+
+    &--focused,
+    :focus {
+      border: 1px solid $primary-color;
+      outline: 3px solid rgba($primary-color, 0.3);
+    }
+  }
+
+  &__error {
+    border: 1px solid $error-color;
+    outline: 3px solid rgba($error-color, 0.5);
+
+    &--text {
+      color: $error-color;
+      font-size: $size-s;
+      padding-left: $size-xs;
+      padding-top: 0.375rem;
+    }
+  }
+}
+</style>
