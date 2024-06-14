34457a19cdc67988f1ece6f344292c9c2bcfbe4e
bruhGrassi <brunagm88@gmail.com>
2024-06-14 12:34:04 -0300

    chore: add nodemon

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Bruna!");
+  res.send("Hello Bruna!!!");
 });
 
 app.listen(port, () => {
