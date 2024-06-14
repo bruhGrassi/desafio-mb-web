7cd089131a7dc45a05337a0dc3121a3a13bebc6a
bruhGrassi <brunagm88@gmail.com>
2024-06-14 14:03:58 -0300

    chore: Add Password

---
+++ src/client/views/Password.vue
@@ -0,0 +1,18 @@
+<script setup>
+import InputField from "@/components/InputField.vue";
+
+const props = defineProps({
+  entity: Object,
+  errors: Object,
+});
+</script>
+
+<template>
+  <InputField
+    type="password"
+    id="password"
+    label="Sua senha"
+    v-model="entity.password"
+    :error="errors.password"
+  />
+</template>
